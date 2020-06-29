5bf02c919358fe70a11034efa5ae73fef0698a50
Marcos Volke <marcos@sizex.com.br>
2020-06-29 08:13:33 -0300

    Incluindo autenticação JWT

---
+++ src/services/AuthenticateUserService.ts
@@ -0,0 +1,51 @@
+import { getRepository } from 'typeorm';
+import { compare } from 'bcryptjs'; // compare compara uma senha não criptografada com uma senha criptografada
+import { sign } from 'jsonwebtoken';
+
+import User from '../models/User';
+
+interface RequestDTO {
+    email: string;
+    password: string;
+}
+
+interface ResponseDTO {
+    user: User;
+    token: string;
+}
+
+class AuthenticateUserService {
+    public async execute({
+        email,
+        password,
+    }: RequestDTO): Promise<ResponseDTO> {
+        const usersRepository = getRepository(User);
+
+        const user: User | undefined = await usersRepository.findOne({
+            where: { email },
+        });
+        if (!user) {
+            throw new Error('Incorret email/password combination.');
+        }
+
+        const passwordMatched: boolean = await compare(password, user.password);
+        if (!passwordMatched) {
+            throw new Error('Incorret email/password combination.');
+        }
+
+        // 1º parâmetro é o payload são infos do usuário não seguras, id, permissões, etc. Fácil de descriptografar.
+        // 2º segundo parâmetro é um secret (senha) nossa - usar md5 online pra gerar uma chave
+        // 3º configurações do token - não deixar expiração eterna e 1 dia seria muito também, deixar minutos ou horas
+        const token = sign({}, 'c7c1670df8bb2eb863ff5109735500d1', {
+            subject: user.id,
+            expiresIn: '1d',
+        });
+
+        return {
+            user,
+            token,
+        };
+    }
+}
+
+export default AuthenticateUserService;
