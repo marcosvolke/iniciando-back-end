50dd53402b0e9a9904a8ee58044bb5271d9d855f
Marcos Volke <marcos@sizex.com.br>
2020-07-01 10:01:35 -0300

    Upload de arquivos configurado e testado na rota de usuário

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,8 +1,14 @@
 import { Router } from 'express';
+import multer from 'multer';
+
+import uploadConfig from '../config/upload';
+
+import ensureAuthenticated from '../middlewares/ensuredAuthenticated';
 
 import CreateUserService from '../services/CreateUserService';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
     try {
@@ -24,5 +30,15 @@ usersRouter.post('/', async (request, response) => {
         return response.status(400).json({ error: err.message });
     }
 });
+
+usersRouter.patch(
+    '/avatar',
+    ensureAuthenticated,
+    upload.single('avatar'),
+    async (request, response) => {
+        console.log(request.file);
+        return response.json({ message: 'ok' });
+    },
+);
 
 export default usersRouter;
