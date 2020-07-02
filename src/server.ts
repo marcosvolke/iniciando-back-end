43e3a46940cd28c0b02a3b855f412933a27651fc
Marcos Volke <marcos@sizex.com.br>
2020-07-02 10:44:36 -0300

    Criação de Tratamento de Exceções Global no server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
 import 'reflect-metadata';
 
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
+
 import routes from './routes';
 import uploadConfig from './config/upload';
+import AppError from './errors/AppError';
 
 import './database';
 
@@ -11,6 +14,25 @@ const app = express();
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
+
+app.use(
+    // Trocar next por _ e configurar eslint pra quando for underline saber q eu não vou usar o parâmetro
+    // esse parâmetro é obrigatório, por isso tem que fazer essa gambis
+    (err: Error, request: Request, response: Response, _: NextFunction) => {
+        // Se é um erro gerado pela minha aplicação, retorno dessa forma
+        if (err instanceof AppError) {
+            return response.status(err.statusCode).json({
+                status: 'error',
+                message: err.message,
+            });
+        }
+
+        return response.status(500).json({
+            status: 'error',
+            message: 'Internal server error',
+        });
+    },
+);
 
 app.listen(3333, () => {
     // eslint-disable-next-line no-console
