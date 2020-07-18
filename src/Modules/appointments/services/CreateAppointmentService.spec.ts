e9e02280d01b7a6d60a2e77bc5e278004bf8318f
Marcos Volke <marcos@sizex.com.br>
2020-07-18 09:49:06 -0300

    Testes com jest: configuração e ínicio do primeiro teste

---
+++ src/Modules/appointments/services/CreateAppointmentService.spec.ts
@@ -0,0 +1,12 @@
+import CreateAppointmentService from './CreateAppointmentService';
+
+// categorizo todos os testes dentro desse arquivo
+describe('CreateAppointment', () => {
+    it('should be able to create a new appointment', () => {
+        expect(1 + 2).toBe(3);
+    });
+
+    it('should not be able to create to appointment on the same time', () => {
+        expect(1 + 2).toBe(3);
+    });
+});
