ddfb4de82ea5ddc5eefc4baa9e95895847071688
Marcos Volke <marcos@sizex.com.br>
2020-07-18 10:19:21 -0300

    Criação de Teste do CreateAppointmentService - foram necessárias mais configs para o jest funcionar - copiar

--- src/Modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/Modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,9 +1,22 @@
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
 import CreateAppointmentService from './CreateAppointmentService';
 
 // categorizo todos os testes dentro desse arquivo
 describe('CreateAppointment', () => {
-    it('should be able to create a new appointment', () => {
-        expect(1 + 2).toBe(3);
+    it('should be able to create a new appointment', async () => {
+        const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+
+        const createAppointment = new CreateAppointmentService(
+            fakeAppointmentsRepository,
+        );
+
+        const appointment = await createAppointment.execute({
+            date: new Date(),
+            provider_id: '156156156',
+        });
+
+        expect(appointment).toHaveProperty('id');
+        expect(appointment.provider_id).toBe('156156156');
     });
 
     it('should not be able to create to appointment on the same time', () => {
