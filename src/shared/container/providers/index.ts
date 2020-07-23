6b97aef7dbd42f91a1cfae9f5d0794635e612e7c
Marcos Volke <marcos@sizex.com.br>
2020-07-23 17:59:45 -0300

    TDD na prática com envio de email de recuperação de senha

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -1,9 +1,13 @@
+import { FakeMailProvider } from '@shared/container/providers/MailProvider/fakes/FakeMailProvider';
 import { container } from 'tsyringe';
 
 import IStorageProvider from './StorageProvider/models/IStorageProvider';
 import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider';
+// import IMailProvider from './MailProvider/models/IMailProvider';
 
 container.registerSingleton<IStorageProvider>(
     'StorageProvider',
     DiskStorageProvider,
 );
+
+// container.registerSingleton<IMailProvider>('MailProvider', );
