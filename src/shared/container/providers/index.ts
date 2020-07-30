947aa74132923e32f8d34cb823dc7631ef1dd16f
Marcos Volke <marcos@sizex.com.br>
2020-07-29 21:39:54 -0300

    Implementação do envio de email em modo de dev

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -1,13 +1,19 @@
 import { container } from 'tsyringe';
 
+import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
+import EtherealMailProvider from './MailProvider/implementations/EtherealMailProvider';
+
 import IStorageProvider from './StorageProvider/models/IStorageProvider';
 import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider';
-// import { FakeMailProvider } from '@shared/container/providers/MailProvider/fakes/FakeMailProvider';
-// import IMailProvider from './MailProvider/models/IMailProvider';
 
 container.registerSingleton<IStorageProvider>(
     'StorageProvider',
     DiskStorageProvider,
 );
 
-// container.registerSingleton<IMailProvider>('MailProvider', );
+// com o register singleton não passou pelo constructor da classe, por isso usamos o registerInstance
+// e continua sendo um singleton, vai executar uma vez só
+container.registerInstance<IMailProvider>(
+    'MailProvider',
+    new EtherealMailProvider(),
+);
