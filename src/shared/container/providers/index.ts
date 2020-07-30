c7e0ce787a9063976fa8810300e9371604eb7cd3
Marcos Volke <marcos@sizex.com.br>
2020-07-30 10:20:47 -0300

    Uso to template mail provider feito anteriormente

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -14,14 +14,15 @@ container.registerSingleton<IStorageProvider>(
     DiskStorageProvider,
 );
 
-// com o register singleton não passou pelo constructor da classe, por isso usamos o registerInstance
-// e continua sendo um singleton, vai executar uma vez só
-container.registerInstance<IMailProvider>(
-    'MailProvider',
-    new EtherealMailProvider(),
-);
-
 container.registerSingleton<IMailTemplateProvider>(
     'MailTemplateProvider',
     HandlebarsMailTemplateProvider,
 );
+
+// com o register singleton não passou pelo constructor da classe, por isso usamos o registerInstance
+// e continua sendo um singleton, vai executar uma vez só
+container.registerInstance<IMailProvider>(
+    'MailProvider',
+    container.resolve(EtherealMailProvider),
+); // alterado para container.resolve ao invés somente dar um new pra resolver injeção de dependência do template dentro do mail provider
+// e a ordem das injeções influencia quando uma depende da outra: caso do mail q usa o template
