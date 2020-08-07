fd70b2fa3c58f71f1aefe7cfa943a47643addfb4
Marcos Volke <marcos@sizex.com.br>
2020-08-06 22:20:53 -0300

    Criação de entidade e repositório do MongoDB de Notifications

---
+++ src/Modules/notifications/infra/typeorm/repositories/NotificationsRepository.ts
@@ -0,0 +1,30 @@
+import { getMongoRepository, MongoRepository } from 'typeorm';
+
+import INotificationsRepository from '@modules/notifications/repositories/INotificationsRepository';
+import ICreateNotificationDTO from '@modules/notifications/dtos/ICreateNotificationDTO';
+import Notification from '../schemas/Notification';
+
+class NotificationsRepository implements INotificationsRepository {
+    private OrmRepository: MongoRepository<Notification>;
+
+    constructor() {
+        this.OrmRepository = getMongoRepository(Notification, 'mongo');
+        // como não é a conexão default, tem q passar o nome informado no ormconfig.json no segundo parâmetro
+    }
+
+    public async create({
+        content,
+        recipient_id,
+    }: ICreateNotificationDTO): Promise<Notification> {
+        const notification = this.OrmRepository.create({
+            content,
+            recipient_id,
+        });
+
+        await this.OrmRepository.save(notification);
+
+        return notification;
+    }
+}
+
+export default NotificationsRepository;
