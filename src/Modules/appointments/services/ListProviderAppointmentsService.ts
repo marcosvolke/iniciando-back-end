282da567d9e7bb3db0b53a8736ac2d0ea69983df
Marcos Volke <marcos@sizex.com.br>
2020-08-15 11:39:29 -0300

    Configuracao e Uso do Cache com Redis

--- src/Modules/appointments/services/ListProviderAppointmentsService.ts
+++ src/Modules/appointments/services/ListProviderAppointmentsService.ts
@@ -1,5 +1,7 @@
 import { injectable, inject } from 'tsyringe';
 
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
+
 import IAppointmentsRepository from '../repositories/iAppointmentsRepository';
 import Appointment from '../infra/typeorm/entities/Appointment';
 
@@ -15,6 +17,9 @@ class ListProviderAppointmentsService {
     constructor(
         @inject('AppointmentsRepository')
         private appointmentsRepository: IAppointmentsRepository,
+
+        @inject('CacheProvider')
+        private cacheProvider: ICacheProvider,
     ) {}
 
     public async execute({
@@ -23,6 +28,10 @@ class ListProviderAppointmentsService {
         month,
         day,
     }: IRequestDTO): Promise<Appointment[]> {
+        const cacheData = await this.cacheProvider.recover('fafssfs');
+
+        console.log(cacheData);
+
         const appointments = await this.appointmentsRepository.findAllInDayFromProvider(
             {
                 provider_id,
@@ -31,6 +40,8 @@ class ListProviderAppointmentsService {
                 day,
             },
         );
+
+        // await this.cacheProvider.save('fafssfs', 'sfsafasfsaf');
 
         return appointments;
     }
