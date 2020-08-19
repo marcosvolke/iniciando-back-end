bc70e7c145bcb315e77b02bf2c0a2387137dedf1
Marcos Volke <marcos@sizex.com.br>
2020-08-18 22:33:13 -0300

    Retornar appointments sem senha e com avatar do user usando classtoclass - atencao com save no cache

--- src/Modules/appointments/infra/http/controllers/ProviderAppointmentsController.ts
+++ src/Modules/appointments/infra/http/controllers/ProviderAppointmentsController.ts
@@ -1,6 +1,7 @@
 // index, show, create, update, delete são os métodos possíveis
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
+import { classToClass } from 'class-transformer';
 
 import ListProviderAppointmentsService from '@modules/appointments/services/ListProviderAppointmentsService';
 
@@ -22,6 +23,6 @@ export default class ProviderAppointmentsController {
             day: Number(day),
         });
 
-        return response.json(appointments);
+        return response.json(classToClass(appointments));
     }
 }
--- src/Modules/appointments/services/ListProviderAppointmentsService.ts
+++ src/Modules/appointments/services/ListProviderAppointmentsService.ts
@@ -1,4 +1,5 @@
 import { injectable, inject } from 'tsyringe';
+import { classToClass } from 'class-transformer';
 
 import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 
@@ -46,7 +47,7 @@ class ListProviderAppointmentsService {
 
             // console.log('query');
 
-            await this.cacheProvider.save(cacheKey, appointments);
+            await this.cacheProvider.save(cacheKey, classToClass(appointments));
         }
 
         return appointments;
